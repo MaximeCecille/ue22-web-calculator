91d9847cd315a73b9350dacc5795097901826504
Maxime Cécille <maxime.cecille@etu.minesparis.psl.eu>
2023-03-01 16:20:40 +0100

    affichage du chiffre sur lequel on appuie

--- subject/script.js
+++ subject/script.js
@@ -1,3 +1,57 @@
+// Maxime Cécille
+document.addEventListener("DOMContentLoaded", () => { 
+    const calculator = document.getElementsByClassName("calculator")[0]
+    const keys = document.getElementsByClassName("calculator_key")
+    disp = document.getElementsByClassName("calculator__display")[0]
+
+    calculator.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = e.target.action
+        if (!action)  {
+           nombre = key.dataset.columns
+           let chiffre = key.textContent
+           disp.textContent = chiffre
+        }
+        // Do something
+    }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+)
 // NOTE: 
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
